12347df3280f67b3e68c5c54950ae4b55840ae08
Misawai <69676474+Misawai@users.noreply.github.com>
2021-03-12 17:16:13 +0800

    new things

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from flask import Flask
+from flask import Flask, request,  render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
 
-app=Flask()
+app=Flask(__name__)
+bootstrap=Bootstrap(app)
+
